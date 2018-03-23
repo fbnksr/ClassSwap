762bb7f12279628ca860a618da5f2fc2ab14ec11
Daniel <dgonz371@gmail.com>
2018-03-23 14:42:20 -0400

    Added user validation

--- database.js
+++ database.js
@@ -21,12 +21,32 @@ con.connect(function(err) {
     console.log(result);
   });
 });
-/*
- var addUser  = function (username , password)  {
 
+ var addUser  = function (email , password, firstname , lastname)  {
+   var sql = squel.insert()
+    .into("SEUSER")
+    .set("Email", email)
+    .set("first_name", firstname)
+    .set("last_name", lastname)
+    .set("password", password)
+    con.query (sql.toString(), function(err,result) {
+      if(err) throw err;
+      console.log("query executed");
+      console.log(result);
+    });
+}
+
+var validateUser = function (email, password) {
+  var sql = squel.select()
+    .from("SEUSER")
+    .where("Email = \'{0}\'  and password = \'{1}\.".format(email,password))
+    con.query (sql.toString(), function(err,result) {
+      if(err) throw err;
+      console.log("query executed");
+      console.log(result);
+    });
 }
 
 module.exports = {
   addUser: addUser
 }
-*/
--- index.js
+++ index.js
@@ -7,7 +7,10 @@ app.use(express.static('public'))
 
 app.use(bodyParser.json())
 app.post('/createUser', (req, res) => {
-  database.addUser(req.body.username,req.body.password)
+  //database.addUser(req.body.username,req.body.password)
+})
+app.post('/login', (req, res) => {
+  database.validateUser(req.body.email,req.body.password)
 })
 app.listen(7555, () => {
   console.log('Server running on http://localhost:7555')
